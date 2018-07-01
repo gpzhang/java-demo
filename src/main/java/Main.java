b894366687e78bddf0eedc410f0c25bd668b5276
haishen <haishen@wacai.com>
2018-07-01 20:10:52 +0800

    Java学习demo

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+/**
+ * @author haishen
+ * @date 2018/5/21
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("123");
+    }
+}
