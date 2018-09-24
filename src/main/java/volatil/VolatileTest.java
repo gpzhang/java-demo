41346747d4e227e5ff0cdd7d4964d8901ba081a2
haishen <haishen@wacai.com>
2018-09-24 15:42:17 +0800

    volatile

---
+++ src/main/java/volatil/VolatileTest.java
@@ -0,0 +1,25 @@
+package volatil;
+
+/**
+ * @author haishen
+ * @date 2018/9/13
+ */
+public class VolatileTest {
+    private static boolean isOver = false;
+    static int i = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new Runnable() {
+            int i=0;
+            @Override
+            public void run() {
+                while (!isOver) {
+                }
+                System.out.println("结束");
+            }
+        });
+        thread.start();
+        Thread.sleep(3000);
+        isOver = true;
+    }
+}
