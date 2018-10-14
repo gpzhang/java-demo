8308e245109cb9b79ec23c256dba3d07f0d4fef9
haishen <haishen@wacai.com>
2018-10-14 20:06:38 +0800

    并发编程

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 import spi.Log;
 
-import java.util.Iterator;
-import java.util.ServiceLoader;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 
 /**
  * @author haishen
@@ -10,6 +12,30 @@ import java.util.ServiceLoader;
 public class Main {
 
     public static void main(String[] args) {
+
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(123);
+        arrayList.remove((Object) 2);
+        arrayList.sort(null);
+        Vector vector = new Vector();
+        vector.add(122);
+        vector.get(0);
+        LinkedList linkedList = new LinkedList();
+
+        HashSet hashSet = new HashSet();
+        hashSet.add("123");
+        TreeSet treeSet = new TreeSet();
+        treeSet.add("234");
+
+        List<String> strings = Arrays.asList("6", "1", "3", "1", "2");
+        Collections.sort(strings);//sort方法在这里
+        for (String string : strings) {
+            System.out.println(string);
+        }
+
+        ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap(10);
+
+
         ServiceLoader<Log> serviceLoader = ServiceLoader.load(Log.class);
         Iterator<Log> iterator = serviceLoader.iterator();
         while (iterator.hasNext()) {
---
+++ src/main/java/collection/CopyOnWriteArrayListDemo.java
@@ -0,0 +1,15 @@
+package collection;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * @author haishen
+ * @date 2018/10/11
+ */
+public class CopyOnWriteArrayListDemo {
+
+    public static void main(String[] args) {
+        CopyOnWriteArrayList list=new CopyOnWriteArrayList();
+        list.add("123");
+    }
+}
