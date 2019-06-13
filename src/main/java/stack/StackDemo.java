da7b73e3e8e5915bb4ccb299b852a41d0fecd2f9
haishen <haishen@wacai.com>
2019-06-13 17:52:29 +0800

    排序算法整理

---
+++ src/main/java/stack/StackDemo.java
@@ -0,0 +1,17 @@
+package stack;
+
+import java.util.Stack;
+
+/**
+ * @author haishen
+ * @date 2019/6/13
+ */
+public class StackDemo {
+
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<String>();
+        stack.push("23");
+        stack.push("45");
+
+    }
+}
