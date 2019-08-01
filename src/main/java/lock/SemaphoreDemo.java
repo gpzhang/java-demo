05d2368052cc1b2a683d334e59ed400da280c1ec
haishen <haishen@wacai.com>
2019-08-01 17:30:46 +0800

    aqs理解

--- src/main/java/lock/SemaphoreDemo.java
+++ src/main/java/lock/SemaphoreDemo.java
@@ -24,6 +24,7 @@ public class SemaphoreDemo {
 
     public static void main(String[] args) {
 
+
         ExecutorService service = Executors.newFixedThreadPool(10);
         for (int i = 0; i < 10; i++) {
             service.execute(() -> {
