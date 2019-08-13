5ede40ad43e01e0b9b2858d3c92d414e7234fd40
haishen <haishen@wacai.com>
2019-08-13 15:41:50 +0800

    异常

---
+++ src/main/java/exception/ExceptionDemo.java
@@ -0,0 +1,36 @@
+package exception;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+/**
+ * @author haishen
+ * @date 2019/8/13
+ * 异常体系
+ * <p>
+ * RuntimeException 是不可以检查的异常，也就是编译代码的过程中编译器检查不出来，也不会去检查
+ * Exception 体系下除了RuntimeException其他的都是可以在代码编译过程中进行异常检查的，
+ * 所以我们的代码中要对这类异常显示的进行异常处理
+ * <p>
+ * trycatch抛出的异常时，需要生成异常的栈追踪，
+ * 生成现场快照，消耗系统资源
+ */
+public class ExceptionDemo {
+    public static void main(String[] args) {
+        //RuntimeException，可以不出来，代码也能编译通过
+        int i = 10 / 0;
+        try {
+            i = 10 / 0;
+        } catch (ArithmeticException throwable) {
+            throwable.printStackTrace();
+        }
+        File file = new File("/Users/haishen/data/tm.txt");
+        try {
+            //非RuntimeException，所以需要显示处理异常
+            FileInputStream fileInputStream = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/keyword/VolatileTest.java
+++ src/main/java/keyword/VolatileTest.java
@@ -5,22 +5,21 @@ package keyword;
  * @date 2018/9/13
  */
 public class VolatileTest {
-    private static boolean isOver = false;
-    static int i = 0;
+
+    private static volatile boolean isOver = false;
 
     public static void main(String[] args) throws InterruptedException {
         Thread thread = new Thread(new Runnable() {
-            int i = 0;
-
             @Override
             public void run() {
                 while (!isOver) {
                 }
-                System.out.println("结束");
+                System.out.println("线程运行结束");
             }
         });
         thread.start();
         Thread.sleep(3000);
         isOver = true;
+        System.out.println("已经修改isOver:" + isOver);
     }
 }
